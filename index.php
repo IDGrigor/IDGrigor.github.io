9a6d419bfa8faea25ae64a2cbbdcd73072e1d437
IDGrigor <37560965+IDGrigor@users.noreply.github.com>
2019-04-29 23:39:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="style/style.css">
+<title></title>
+</head>
+<body>
+	<main>
+		<div class="container">
+			
+		</div>
+	</main>
+</body>
+</html>
 <?php 
-if ($_POST['name']='тахикардия') {
+if (($_GET["illness"])== "тахикардия") {
 	print " тахикардия";
 }
-if ($_POST['name']='ишемия') {
+if (($_GET["illness"])== "ишемия") {
 	print " ишемия";
 }
 
